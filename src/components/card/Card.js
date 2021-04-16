da2d568c424c82ec72a0e3eae3748dd7c36a7627
Seongseokwon <pam9411@naver.com>
2021-04-16 17:41:10 +0900

    add gh-pages

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Card.scss';
 import Review from './Review';
+
 function Card(props) {
   const { star, review, position, writer } = props;
   return position === 'vertical' ? (
@@ -9,7 +10,7 @@ function Card(props) {
     // 별점, 후기영역
 
     <div className={`card ${position}`}>
-      <img src="../images/logo.png" alt="itemImage" />
+      <img src="/images/logo.png" alt="itemImage" />
       <div className="card_content">
         <h3 className="label">Card Label</h3>
         <h2 className="title">Card Title</h2>
